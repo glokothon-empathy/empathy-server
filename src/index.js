566d2fab28485f63be73c76113bf73b4ce94cab5
kidkkr <kidkkr@naver.com>
2017-07-22 19:10:02 +0900

    static 설정

--- src/index.js
+++ src/index.js
@@ -8,8 +8,12 @@ import ideasRouter from './router/ideas';
 const app = express();
 const PORT = 8001;
 
+// 미들웨어
 app.use(bodyPaser.json());
 app.use(expressLogger({ path: path.resolve(__dirname, '../express.log') }));
+
+// 라우터 설정
+app.use(express.static(path.resolve(__dirname, '../public')));
 app.use('/ideas', ideasRouter);
 
 app.listen(PORT, (err) => {
