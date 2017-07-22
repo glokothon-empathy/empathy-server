b7465f92680f306b4286ceda6b14f53beb58e7cd
Dong Hee Kim <opklnm102@gmail.com>
2017-07-22 17:43:20 +0900

    Add: 아이디어 댓글 등록 API 추가

--- src/router/ideas.js
+++ src/router/ideas.js
@@ -34,4 +34,31 @@ ideasRouter.get('/:id', (req, res) => {
 
 });
 
+/******* 아이디어 댓글 *******/
+// 아이디어 댓글 추가
+ideasRouter.post('/:id/comments', (req, res) => {
+  const { contents, user_id, idea_id } = req.body;
+  connection.query(
+    `INSERT INTO idea_comment (contents, user_id, idea_id) VALUES("${contents}", ${user_id}, ${idea_id});`,
+    (err, results, fields) => {
+      if (err) {
+        rres.status(400).json({});
+        console.log(err.stack);
+      }
+      res.status(200).json(results);
+    }
+  );
+});
+
+// 리스트 - GET /ideas/{id}/comments
+// 아이디어 댓글 리스트
+
+// 수정 - PUT /ideas/{id}/comments/{id}
+// 아이디어 댓글 수정
+
+// 삭제 - DELETE /ideas/{id}/comments/{id}
+// 아이디어 댓글 삭제
+
+
+
 export default ideasRouter;
