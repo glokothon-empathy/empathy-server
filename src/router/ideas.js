99438d8ab90b04215ce4e23e1ed319dc562b5b61
Dong Hee Kim <opklnm102@gmail.com>
2017-07-22 20:31:24 +0900

    Fix: 코드 정

--- src/router/ideas.js
+++ src/router/ideas.js
@@ -10,9 +10,9 @@ ideasRouter.post('/', (req, res) => {
 
   req.app.get('pool').query(
     `INSERT INTO idea (contents, user_id) VALUES("${contents}", ${userId});`,
-    (error, results, fields) => {
-      if (error) {
-        console.log(error.stack);
+    (err, results, fields) => {
+      if (err) {
+        console.log(err.stack);
       }
       res.json(results);
     }
@@ -26,11 +26,11 @@ ideasRouter.put('/:idea_id', (req, res) => {
   const userId = req.app.get('user_id');
 
   req.app.get('pool').query(
-    `update idea set contents="${contents}" where id=${ideaId};`,
-    (error, results, fields) => {
-      if (error) {
+    `UPDATE idea SET contents = "${contents}" WHERE id = ${ideaId};`,
+    (err, results, fields) => {
+      if (err) {
         res.status(500).json({ msg: 'FAIL' });
-        console.log(error.stack);
+        console.log(err.stack);
       }
       res.status(200).json(results);
     }
@@ -43,11 +43,11 @@ ideasRouter.delete('/:idea_id', (req, res) => {
   const userId = req.app.get('user_id');
 
   req.app.get('pool').query(
-    `delete from idea where id=${ideaId};`,
-    (error, results, fields) => {
-      if (error) {
+    `DELETE FROM idea WHERE id = ${ideaId};`,
+    (err, results, fields) => {
+      if (err) {
         res.status(500).json({ msg: 'FAIL' });
-        console.log(error.stack);
+        console.log(err.stack);
       }
       res.status(200).json(results);
     }
@@ -59,11 +59,11 @@ ideasRouter.get('/', (req, res) => {
   const userId = req.app.get('user_id');
 
   req.app.get('pool').query(
-    `select * from idea;`,
-    (error, results, fields) => {
-      if (error) {
+    `SELECT (id, contents, empathy_count, join_count) FROM idea;`,
+    (err, results, fields) => {
+      if (err) {
         res.status(500).json({ msg: 'FAIL' });
-        console.log(error.stack);
+        console.log(err.stack);
       }
       res.status(200).json(results);
     }
@@ -71,15 +71,16 @@ ideasRouter.get('/', (req, res) => {
 });
 
 // 아이디어 상세 조회
-ideasRouter.get('/:id', (req, res) => {
+ideasRouter.get('/:idea_id', (req, res) => {
   const userId = req.app.get('user_id');
+  const ideaId = req.params.idea_id;
 
   req.app.get('pool').query(
-    `select * from idea where id=${req.params.id};`,
-    (error, results, fields) => {
-      if (error) {
+    `SELECT (id, contents, empathy_count, join_count) FROM idea WHERE id = ${ideaId};`,
+    (err, results, fields) => {
+      if (err) {
         res.status(500).json({ msg: 'FAIL' });
-        console.log(error.stack);
+        console.log(err.stack);
       }
       res.status(200).json(results[0]);
     }
@@ -88,15 +89,16 @@ ideasRouter.get('/:id', (req, res) => {
 
 /******* 아이디어 댓글 *******/
 // 아이디어 댓글 추가
-ideasRouter.post('/:id/comments', (req, res) => {
+ideasRouter.post('/:idea_id/comments', (req, res) => {
   const userId = req.app.get('user_id');
+  const ideaId = req.params.idea_id;
+  const { contents } = req.body;
 
-  const { contents, user_id, idea_id } = req.body;
   req.app.get('pool').query(
-    `INSERT INTO idea_comment (contents, user_id, idea_id) VALUES("${contents}", ${user_id}, ${idea_id});`,
+    `INSERT INTO idea_comment (contents, user_id, idea_id) VALUES("${contents}", ${userId}, ${ideaId});`,
     (err, results, fields) => {
       if (err) {
-        res.status(400).json({});
+        res.status(500).json({ msg: 'FAIL' });
         console.log(err.stack);
       }
       res.status(200).json(results);
@@ -115,7 +117,7 @@ ideasRouter.put('/:idea_id/comments/:comment_id', (req, res) => {
     `UPDATE idea_comment SET contents = "${contents}" WHERE id = ${commentId};`,
     (err, results, fields) => {
       if (err) {
-        res.status(400).json({});
+        res.status(500).json({ msg: 'FAIL' });
         console.log(err.stack);
       }
       res.status(200).json(results);
@@ -133,7 +135,7 @@ ideasRouter.delete('/:idea_id/comments/:comment_id', (req, res) => {
     `DELETE FROM idea_comment WHERE id = ${commentId} AND idea_id = ${ideaId};`,
     (err, results, fields) => {
       if (err) {
-        res.status(400).json({});
+        res.status(500).json({ msg: 'FAIL' });
         console.log(err.stack);
       }
       res.status(200).json({ msg: 'OK' });
@@ -147,10 +149,10 @@ ideasRouter.get('/:idea_id/comments', (req, res) => {
   const userId = req.app.get('user_id');
 
   req.app.get('pool').query(
-    `SELECT (id, contents, user_id, created_at, updated_at FROM idea_comment WHERE idea_id = ${ideaId};`,
+    `SELECT (id, contents, user_id, created_at, updated_at) FROM idea_comment WHERE idea_id = ${ideaId};`,
     (err, results, fields) => {
       if (err) {
-        res.status(400).json({});
+        res.status(500).json({ msg: 'FAIL' });
         console.log(err.stack);
       }
       res.status(200).json(results);
@@ -160,15 +162,15 @@ ideasRouter.get('/:idea_id/comments', (req, res) => {
 
 /******* 아이디어 공감 *******/
 // 아이디어 공감 추가
-ideasRouter.post('/:id/empathy', (req, res) => {
-  const { idea_id } = req.body;
+ideasRouter.post('/:idea_id/empathy', (req, res) => {
+  const ideaId = req.params.idea_id;
   const userId = req.app.get('user_id');
 
   req.app.get('pool').query(
     `INSERT INTO idea_empathy (user_id, idea_id) VALUES(${user_id}, ${idea_id});`,
     (err, results, fields) => {
       if (err) {
-        res.status(400).json({});
+        res.status(500).json({ msg: 'FAIL' });
         console.log(err.stack);
       }
       res.status(200).json(results);
@@ -177,16 +179,15 @@ ideasRouter.post('/:id/empathy', (req, res) => {
 });
 
 // 아이디어 공감 삭제
-ideasRouter.delete('/:id/empathy/:empathy_id', (req, res) => {
-  const ideaId = req.params.id;
-  const empathyId = req.params.empathy_id;
+ideasRouter.delete('/:idea_id/empathy', (req, res) => {
+  const ideaId = req.params.idea_id;
   const userId = req.app.get('user_id');
 
   req.app.get('pool').query(
     `DELETE FROM idea_empathy WHERE id = ${empathyId} AND idea_id = ${ideaId};`,
     (err, results, fields) => {
       if (err) {
-        res.status(400).json({});
+        res.status(500).json({ msg: 'FAIL' });
         console.log(err.stack);
       }
       res.status(200).json({ msg: 'OK' });
@@ -197,7 +198,6 @@ ideasRouter.delete('/:id/empathy/:empathy_id', (req, res) => {
 /******* 아이디어 참여 *******/
 // 아이디어 참여 추가
 ideasRouter.post('/:idea_id/join', (req, res) => {
-  const { user_id } = req.body;
   const ideaId = req.params.idea_id;
   const userId = req.app.get('user_id');
 
@@ -205,7 +205,7 @@ ideasRouter.post('/:idea_id/join', (req, res) => {
     `INSERT INTO idea_join (user_id, idea_id) VALUES(${user_id}, ${ideaId});`,
     (err, results, fields) => {
       if (err) {
-        res.status(400).json({});
+        res.status(500).json({ msg: 'FAIL' });
         console.log(err.stack);
       }
       res.status(200).json(results);
@@ -214,16 +214,15 @@ ideasRouter.post('/:idea_id/join', (req, res) => {
 });
 
 // 아이디어 참여 삭제
-ideasRouter.delete('/:idea_id/join/:join_id', (req, res) => {
+ideasRouter.delete('/:idea_id/join', (req, res) => {
   const ideaId = req.params.idea_id;
-  const joinId = req.params.join_id;
   const userId = req.app.get('user_id');
 
   connection.query(
-    `DELETE FROM idea_join WHERE id = ${joinId} AND idea_id = ${ideaId};`,
+    `DELETE FROM idea_join WHERE user_id = ${userId} AND idea_id = ${ideaId};`,
     (err, results, fields) => {
       if (err) {
-        res.status(400).json({});
+        res.status(500).json({ msg: 'FAIL' });
         console.log(err.stack);
       }
       res.status(200).json({ msg: 'OK' });
@@ -240,7 +239,7 @@ ideasRouter.get('/:idea_id/join', (req, res) => {
     `SELECT (user.id, user.name) FROM idea_join INNER JOIN user ON user.id = idea_join.user_id WHERE idea_id = ${ideaId};`,
     (err, results, fields) => {
       if (err) {
-        res.status(400).json({});
+        res.status(500).json({ msg: 'FAIL' });
         console.log(err.stack);
       }
       res.status(200).json(results);
