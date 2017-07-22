b487c2d47d16155c58ec15f921c8c7ca09143556
Dong Hee Kim <opklnm102@gmail.com>
2017-07-22 20:10:17 +0900

    Add: 로그인을 가정하기 위해 app에 유저 ID 추가

--- src/index.js
+++ src/index.js
@@ -16,6 +16,9 @@ app.use(bodyPaser.json());
 app.use(expressLogger({ path: path.resolve(__dirname, '../express.log') }));
 app.set('pool', connection);
 
+// Todo: 로그인 한것처럼 가정. 로그인 구현 후 삭제
+app.set('user_id', 1);
+
 // 라우터 설정
 app.use(express.static(path.resolve(__dirname, '../public')));
 app.use('/ideas', ideasRouter);
--- src/router/ideas.js
+++ src/router/ideas.js
@@ -5,9 +5,11 @@ const ideasRouter = express.Router();
 /******* 아이디어 *******/
 // 아이디어 추가
 ideasRouter.post('/', (req, res) => {
-  const { contents, user_id } = req.body;
+  const { contents } = req.body;
+  const userId = req.app.get('user_id');
+
   req.app.get('pool').query(
-    `insert into idea (contents, user_id) values("${contents}", ${user_id});`,
+    `INSERT INTO idea (contents, user_id) VALUES("${contents}", ${userId});`,
     (error, results, fields) => {
       if (error) {
         console.log(error.stack);
@@ -18,10 +20,13 @@ ideasRouter.post('/', (req, res) => {
 });
 
 // 아이디어 수정
-ideasRouter.put('/', (req, res) => {
-  const { id, contents } = req.body;
+ideasRouter.put('/:idea_id', (req, res) => {
+  const { contents } = req.body;
+  const ideaId = req.params.idea_id;
+  const userId = req.app.get('user_id');
+
   req.app.get('pool').query(
-    `update idea set contents="${contents}" where id=${id};`,
+    `update idea set contents="${contents}" where id=${ideaId};`,
     (error, results, fields) => {
       if (error) {
         res.status(500).json({ msg: 'FAIL' });
@@ -33,10 +38,12 @@ ideasRouter.put('/', (req, res) => {
 });
 
 // 아이디어 삭제
-ideasRouter.delete('/', (req, res) => {
-  const { id } = req.body;
+ideasRouter.delete('/:idea_id', (req, res) => {
+  const ideaId = req.params.idea_id;
+  const userId = req.app.get('user_id');
+
   req.app.get('pool').query(
-    `delete from idea where id=${id};`,
+    `delete from idea where id=${ideaId};`,
     (error, results, fields) => {
       if (error) {
         res.status(500).json({ msg: 'FAIL' });
@@ -49,6 +56,8 @@ ideasRouter.delete('/', (req, res) => {
 
 // 아이디어 리스트 조회
 ideasRouter.get('/', (req, res) => {
+  const userId = req.app.get('user_id');
+
   req.app.get('pool').query(
     `select * from idea;`,
     (error, results, fields) => {
@@ -63,6 +72,8 @@ ideasRouter.get('/', (req, res) => {
 
 // 아이디어 상세 조회
 ideasRouter.get('/:id', (req, res) => {
+  const userId = req.app.get('user_id');
+
   req.app.get('pool').query(
     `select * from idea where id=${req.params.id};`,
     (error, results, fields) => {
@@ -78,6 +89,8 @@ ideasRouter.get('/:id', (req, res) => {
 /******* 아이디어 댓글 *******/
 // 아이디어 댓글 추가
 ideasRouter.post('/:id/comments', (req, res) => {
+  const userId = req.app.get('user_id');
+
   const { contents, user_id, idea_id } = req.body;
   req.app.get('pool').query(
     `INSERT INTO idea_comment (contents, user_id, idea_id) VALUES("${contents}", ${user_id}, ${idea_id});`,
@@ -95,7 +108,9 @@ ideasRouter.post('/:id/comments', (req, res) => {
 ideasRouter.put('/:idea_id/comments/:comment_id', (req, res) => {
   const ideaId = req.params.idea_id;
   const commentId = req.params.comment_id;
-  const { contents, user_id } = req.body;
+  const { contents } = req.body;
+  const userId = req.app.get('user_id');
+
   req.app.get('pool').req.app.get('pool').query(
     `UPDATE idea_comment SET contents = "${contents}" WHERE id = ${commentId};`,
     (err, results, fields) => {
@@ -112,6 +127,8 @@ ideasRouter.put('/:idea_id/comments/:comment_id', (req, res) => {
 ideasRouter.delete('/:idea_id/comments/:comment_id', (req, res) => {
   const ideaId = req.params.idea_id;
   const commentId = req.params.comment_id;
+  const userId = req.app.get('user_id');
+
   req.app.get('pool').query(
     `DELETE FROM idea_comment WHERE id = ${commentId} AND idea_id = ${ideaId};`,
     (err, results, fields) => {
@@ -127,6 +144,8 @@ ideasRouter.delete('/:idea_id/comments/:comment_id', (req, res) => {
 // 아이디어 댓글 리스트
 ideasRouter.get('/:idea_id/comments', (req, res) => {
   const ideaId = req.params.idea_id;
+  const userId = req.app.get('user_id');
+
   req.app.get('pool').query(
     `SELECT (id, contents, user_id, created_at, updated_at FROM idea_comment WHERE idea_id = ${ideaId};`,
     (err, results, fields) => {
@@ -142,7 +161,8 @@ ideasRouter.get('/:idea_id/comments', (req, res) => {
 /******* 아이디어 공감 *******/
 // 아이디어 공감 추가
 ideasRouter.post('/:id/empathy', (req, res) => {
-  const { user_id, idea_id } = req.body;
+  const { idea_id } = req.body;
+  const userId = req.app.get('user_id');
 
   req.app.get('pool').query(
     `INSERT INTO idea_empathy (user_id, idea_id) VALUES(${user_id}, ${idea_id});`,
@@ -160,6 +180,7 @@ ideasRouter.post('/:id/empathy', (req, res) => {
 ideasRouter.delete('/:id/empathy/:empathy_id', (req, res) => {
   const ideaId = req.params.id;
   const empathyId = req.params.empathy_id;
+  const userId = req.app.get('user_id');
 
   req.app.get('pool').query(
     `DELETE FROM idea_empathy WHERE id = ${empathyId} AND idea_id = ${ideaId};`,
@@ -178,6 +199,7 @@ ideasRouter.delete('/:id/empathy/:empathy_id', (req, res) => {
 ideasRouter.post('/:idea_id/join', (req, res) => {
   const { user_id } = req.body;
   const ideaId = req.params.idea_id;
+  const userId = req.app.get('user_id');
 
   connection.query(
     `INSERT INTO idea_join (user_id, idea_id) VALUES(${user_id}, ${ideaId});`,
@@ -195,6 +217,7 @@ ideasRouter.post('/:idea_id/join', (req, res) => {
 ideasRouter.delete('/:idea_id/join/:join_id', (req, res) => {
   const ideaId = req.params.idea_id;
   const joinId = req.params.join_id;
+  const userId = req.app.get('user_id');
 
   connection.query(
     `DELETE FROM idea_join WHERE id = ${joinId} AND idea_id = ${ideaId};`,
@@ -211,6 +234,7 @@ ideasRouter.delete('/:idea_id/join/:join_id', (req, res) => {
 // 아이디어 참여 리스트
 ideasRouter.get('/:idea_id/join', (req, res) => {
   const ideaId = req.params.idea_id;
+  const userId = req.app.get('user_id');
 
   connection.query(
     `SELECT (user.id, user.name) FROM idea_join INNER JOIN user ON user.id = idea_join.user_id WHERE idea_id = ${ideaId};`,
