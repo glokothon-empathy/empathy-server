13f5124d7f84739c65a7b21d8f2367e1ac7f7b0b
kidkkr <kidkkr@naver.com>
2017-07-22 17:47:00 +0900

    API GET /ideas/:id

--- src/router/ideas.js
+++ src/router/ideas.js
@@ -19,11 +19,20 @@ ideasRouter.post('/', (req, res) => {
 });
 
 ideasRouter.delete('/', (req, res) => {
-
+  const { id } = req.body;
+  connection.query(
+    `delete from idea where ${id};`,
+    (error, results, fields) => {
+      if (error) {
+        console.log(error.stack);
+      }
+      res.json({ msg: 'OK' });
+    }
+  );
 });
 
 ideasRouter.put('/', (req, res) => {
-
+  
 });
 
 ideasRouter.get('/', (req, res) => {
@@ -31,15 +40,25 @@ ideasRouter.get('/', (req, res) => {
     `select * from idea;`,
     (error, results, fields) => {
       if (error) {
+        res.status(500).json({ msg: 'FAIL' });
         console.log(error.stack);
       }
-      res.json(results);
+      res.status(200).json(results);
     }
   );
 });
 
 ideasRouter.get('/:id', (req, res) => {
-
+  connection.query(
+    `select * from idea where id=${req.params.id};`,
+    (error, results, fields) => {
+      if (error) {
+        res.status(500).json({ msg: 'FAIL' });
+        console.log(error.stack);
+      }
+      res.status(200).json(results[0]);
+    }
+  );
 });
 
 export default ideasRouter;
