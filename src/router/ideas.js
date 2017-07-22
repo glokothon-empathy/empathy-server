5b2ca41409155f1e03706a6edd62bcae0ecb9b4d
Dong Hee Kim <opklnm102@gmail.com>
2017-07-22 18:40:56 +0900

    Add: 아이디어 댓글 리스트 조회 API 추가

--- src/router/ideas.js
+++ src/router/ideas.js
@@ -50,7 +50,7 @@ ideasRouter.post('/:id/comments', (req, res) => {
     `INSERT INTO idea_comment (contents, user_id, idea_id) VALUES("${contents}", ${user_id}, ${idea_id});`,
     (err, results, fields) => {
       if (err) {
-        rres.status(400).json({});
+        res.status(400).json({});
         console.log(err.stack);
       }
       res.status(200).json(results);
@@ -60,6 +60,21 @@ ideasRouter.post('/:id/comments', (req, res) => {
 
 // 리스트 - GET /ideas/{id}/comments
 // 아이디어 댓글 리스트
+ideasRouter.get('/:idea_id/comments/', (req, res) => {
+  const ideaId = req.params.idea_id;
+
+  connection.query(
+    `SELECT (id, contents, user_id, created_at, updated_at FROM idea_comment WHERE idea_id = ${ideaId};`,
+    (err, results, fields) => {
+      if (err) {
+        res.status(400).json({});
+        console.log(err.stack);
+      }
+      res.status(200).json(results);
+    }
+  );
+});
+
 
 // 수정 - PUT /ideas/{id}/comments/{id}
 // 아이디어 댓글 수정
