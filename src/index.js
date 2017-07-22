b2ff8e1235aacef68e77bb6d52390faec9309bae
Dong Hee Kim <opklnm102@gmail.com>
2017-07-23 01:21:28 +0900

    Add: CORS 처리 추가

--- src/index.js
+++ src/index.js
@@ -20,6 +20,19 @@ app.set('pool', connection);
 // Todo: 로그인 한것처럼 가정. 로그인 구현 후 삭제
 app.set('user_id', 1);
 
+var allowCORS = function(req, res, next) {
+  res.header('Acess-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  (req.method === 'OPTIONS') ?
+    res.send(200) :
+    next();
+};
+ 
+// 이 부분은 app.use(router) 전에 추가하도록 하자
+app.use(allowCORS);
+ 
+
 // 라우터 설정
 app.use(express.static(path.resolve(__dirname, '../public')));
 app.use('/ideas', ideasRouter);
